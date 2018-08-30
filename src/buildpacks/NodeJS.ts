6f9e7aab35b8c87dc067e4bd34a07c5ea98e3612
Remie Bolte <r.bolte@gmail.com>
2018-08-30 15:31:48 +0200

    Get the NodeJS version from the package.json

--- src/buildpacks/NodeJS.ts
+++ src/buildpacks/NodeJS.ts
@@ -2,8 +2,10 @@
 
 // ------------------------------------------------------------------------------------------ Dependencies
 
+import * as path from 'path';
 import { ProjectProperties, BuildPack } from '../Interfaces';
 import AbstractBuildPack from './AbstractBuildPack';
+import * as semver from 'semver';
 
 // ------------------------------------------------------------------------------------------ Class
 
@@ -14,7 +16,13 @@ export default class NodeJS extends AbstractBuildPack {
   }
 
   get tag() {
-    return '8';
+    let version = 8;
+    const pkg = require(path.join(process.cwd(), 'package.json'));
+    if (pkg.engines && pkg.engines.node) {
+      const maxVersion = semver.maxSatisfying([ '6.0.0', '7.0.0', '8.0.0', '9.0.0', '10.0.0', '11.0.0' ], pkg.engines.node) || `${version}.0.0`;
+      version = semver.major(maxVersion);
+    }
+    return version.toString();
   }
 
   get body() {
