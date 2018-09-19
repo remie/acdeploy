0d1d5f1dd003841c9b3031a49f642097b09b28b1
Remie Bolte <r.bolte@gmail.com>
2018-09-19 16:49:52 +0200

    Add support for Meteor builds

--- src/buildpacks/NodeJS.ts
+++ src/buildpacks/NodeJS.ts
@@ -2,8 +2,10 @@
 
 // ------------------------------------------------------------------------------------------ Dependencies
 
+import * as fs from 'fs';
 import * as path from 'path';
 import { ProjectProperties, BuildPack } from '../Interfaces';
+import { Utils } from '../lib/Utils';
 import AbstractBuildPack from './AbstractBuildPack';
 import * as semver from 'semver';
 
@@ -30,16 +32,66 @@ export default class NodeJS extends AbstractBuildPack {
     return (pkg.scripts && pkg.scripts.postinstall);
   }
 
-  get body() {
-    let body = `
+  get isMeteor(): boolean {
+    return fs.existsSync(path.join(process.cwd(), '.meteor'));
+  }
 
+  get hasNpmToken(): boolean {
+    return (Utils.properties.options.docker
+      && Utils.properties.options.docker.buildArgs
+      && Utils.properties.options.docker.buildArgs.filter((arg) => arg.name === 'NPM_TOKEN').length > 0);
+  }
+
+  get body() {
+    let body = '';
+
+    if (this.isMeteor) {
+      body += `
+ENV METEOR_ALLOW_SUPERUSER=true
+
+## Install prerequisites
+RUN apt-get update -y; \
+  apt-get install -y \
+  curl;
+
+## Install Meteor
+RUN curl https://install.meteor.com | /bin/sh
+
+`;
+    }
+
+    body += `
 WORKDIR /opt
 COPY ./package.json package.json
 COPY ./package-lock.json package-lock.json
-RUN npm install
+`;
 
+    if (this.hasNpmToken) {
+      body += `
+RUN echo "//registry.npmjs.org/:_authToken=\$\{NPM_TOKEN\}" > .npmrc
+`;
+    }
+
+    if (this.isMeteor) {
+      body += `
+RUN meteor npm install
+`;
+    } else {
+      body += `
+RUN npm install
+`;
+    }
+
+    body += `
 COPY . ./
-    `;
+`;
+
+    if (this.isMeteor) {
+      body += `
+RUN meteor build dist --directory
+RUN (cd dist/bundle/programs/server && npm install)
+`;
+    }
 
     if (this.hasPostInstall) {
       body += `
@@ -48,6 +100,13 @@ RUN npm run postinstall
     }
 
     return body;
+  }
+
+  get dockerignore() {
+    return `
+node_modules
+.npmrc
+`;
   }
 
   get command() {
